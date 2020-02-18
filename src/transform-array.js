74a225e62088c45f0d304126b48e17208b51d27a
arcanar7 <arcanar7@gmail.com>
2020-02-18 12:57:21 +0400

    feat: implement subtask solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,20 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  let result = []
+
+  if (!Array.isArray(arr)) throw new Error()
+
+  if (!arr.length) return result
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "--double-prev")
+      arr[i - 1] !== undefined ? result.push(arr[i - 1]) : null
+    else if (arr[i] === "--double-next")
+      arr[i + 1] !== undefined ? result.push(arr[i + 1]) : null
+    else if (arr[i] === "--discard-prev")
+      arr[i - 1] !== undefined ? result.pop() : null
+    else if (arr[i] === "--discard-next") i++
+    else result.push(arr[i])
+  }
+
+  return result
+}
