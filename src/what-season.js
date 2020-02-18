5399ef5ba3ec354dbf5eb1b771c6a6bc83536446
arcanar7 <arcanar7@gmail.com>
2020-02-18 10:40:45 +0400

    feat: implement subtask solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,17 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if (!date) return "Unable to determine the time of year!"
+
+  if (!(date instanceof Date)) throw new Error()
+
+  try {
+    date.valueOf()
+  } catch {
+    throw new Error()
+  }
+
+  if (date.getMonth() === 0 || date.getMonth() === 1 || date.getMonth() === 11)
+    return "winter"
+  else if (date.getMonth() >= 2 && date.getMonth() <= 4) return "spring"
+  else if (date.getMonth() >= 5 && date.getMonth() <= 7) return "summer"
+  else if (date.getMonth() >= 8 && date.getMonth() <= 10) return "autumn"
+}
