9a4df4d26965e70bc5847cbb9617fe461e4df73c
arcanar7 <arcanar7@gmail.com>
2020-02-18 15:03:44 +0400

    feat: implement subtask solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,15 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
-};
+  calculateDepth(arr) {
+    let depth = 1
+
+    if (Array.isArray(arr)) {
+      for (let item of arr) {
+        if (Array.isArray(item)) {
+          depth = Math.max(this.calculateDepth(item) + 1, depth)
+        }
+      }
+
+      return depth
+    } else return 0
+  }
+}
