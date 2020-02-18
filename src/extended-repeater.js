773faf359ad8dd25bcaaa646802c79ac9c047fc2
arcanar7 <arcanar7@gmail.com>
2020-02-18 15:47:02 +0400

    feat: implement subtask solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,21 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+  const repeatTimes = options.repeatTimes || 1
+  const separator = options.separator || "+"
+  const addition = options.addition !== undefined ? options.addition : ""
+  const additionRepeatTimes = options.additionRepeatTimes || 1
+  const additionSeparator = options.additionSeparator || "|"
+
+  let resultAddition = ""
+  for (let i = 1; i < additionRepeatTimes; i++) {
+    resultAddition = `${resultAddition}${addition}${additionSeparator}`
+  }
+  resultAddition += addition
+
+  let resultString = ""
+  for (let i = 1; i < repeatTimes; i++) {
+    resultString = `${resultString}${str}${resultAddition}${separator}`
+  }
+  resultString += `${str}${resultAddition}`
+
+  return resultString
+}
